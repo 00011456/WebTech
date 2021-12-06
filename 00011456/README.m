ee26dc15b7175180ea06d9de104bb983397bfa6d
00011456 <92201961+00011456@users.noreply.github.com>
2021-12-06 23:37:45 +0500

    Create README.m

---
+++ 00011456/README.m
@@ -0,0 +1,20 @@
+# Restaurant by 00011456
+
+This website was created to fulfill Web Technology module’s requirements and does not represent an actual company or service
+
+This app can be found at [Netlify](https://00011456.netlify.app/). 
+
+## Overview
+This website shows dynamic data for users where they have cart, order, contacts, delivery, bar pages. [Design](https://www.figma.com/file/V0hwboJ0Xh9Eilr6Z7ciZf/diner-(Copy)-(Copy)?node-id=0%3A1) for the project was taken on open-source platform.
+
+## 🔨 Project`s Stack
+- HTML (Create the structure and content of the web pages).
+- CSS (Styling of the web pages).
+- BEM Methodology.
+- JAVASCRIPT (Interactivity, dom, validation, animation).
+- Figma.
+
+## Setting Up Your Local Environment
+If you want just look at the final version go to - [Netlify](https://00011456.netlify.app/).                                                              
+If you wan to play around, follow some steps:
+                                                             
